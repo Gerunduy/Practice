5ff301ae0197015c27661fd79c4845c0bc372a4d
kartoshkka <kartoshkka@list.ru>
2017-07-10 17:02:27 +0700

    Add entity

--- Expertise/ExpertiseWPFApplication/MainWindow.xaml.cs
+++ Expertise/ExpertiseWPFApplication/MainWindow.xaml.cs
@@ -20,14 +20,45 @@ namespace ExpertiseWPFApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
         public MainWindow()
         {
             InitializeComponent();
+            client.GetListGRNTICompleted += Client_GetListGRNTICompleted;
+            client.GethelloCompleted += Client_GethelloCompleted;
+        }
+
+        private void Client_GethelloCompleted(object sender, ServiceReference1.GethelloCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                
+                MessageBox.Show(e.Result);
+            }
+
+
+            else
+                MessageBox.Show(e.Error.Message);
+        }
+
+        private void Client_GetListGRNTICompleted(object sender, ServiceReference1.GetListGRNTICompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                dataGrid1.ItemsSource = e.Result.ToList();
+
+            }
+
+
+            else
+                MessageBox.Show(e.Error.Message);
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("test");
+            //client.GethelloAsync();
+            client.GetListGRNTIAsync();
         }
     }
 }
