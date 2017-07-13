e1a81880d36431f1fc608eb5cc219ebcd6dd09c5
Gerunduy <sr977@mail.ru>
2017-07-13 21:09:14 +0700

    s5

--- Expertise/ExpertiseWCFService/IService1.cs
+++ Expertise/ExpertiseWCFService/IService1.cs
@@ -191,6 +191,36 @@ namespace ExpertiseWCFService
         // TODO: Добавьте здесь операции служб
     }
 
+    public class ExpertsWithCountExpertise
+    {
+        public int number { get; set; }
+        public int id_expert { get; set; }
+        public string surname_expert { get; set; }
+        public string name_expert { get; set; }
+        public string patronymic_expert { get; set; }
+        public string FIO { get; set; }
+        public string job_expert { get; set; }//место работы
+        public string post_expert { get; set; }//должность
+        public string degree_expert { get; set; }//степень
+        public string rank_expert { get; set; }//звание
+        public string degree_rank_expert { get; set; }//звание + степень
+        public Boolean delete_expert { get; set; }//удален\активен
+        public string contacts_expert { get; set; }
+        public int countexpertise { get; set; }
+        public List<FiledsOfScience> ListFOS { get; set; }
+    }
+
+    public class Expertise_Expert
+    {
+        public int number { get; set; }
+        public int id_expertise { get; set; }
+        public string name_expertise { get; set; }
+        public string name_fos { get; set; }
+        public DateTime date_expertise { get; set; }
+        public List<string> victory_project { get; set; }
+        public string status_expertise { get; set; }
+    }
+
 
     // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
     [DataContract]
